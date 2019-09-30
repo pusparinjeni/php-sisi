1d0aa0d21be4ca80c7d95bb28e89f5320a6208ee
Puspa Rinjeni <pusparinjeni@gmail.com>
2019-09-30 20:15:14 +0700

    Project Update

--- ProjectSeptember.php
+++ ProjectSeptember.php
@@ -2,7 +2,14 @@
 
 class barang{
     //property
-    public nama_barang, harga_barang, stok_barang;
+    public $nama_barang, $harga_barang, $stok_barang;
+    
+    
+    //konstanta
+        const stok_barang = 0;
+    function showStok(){
+        echo self::stok_barang;
+    }
     
     //method
     function tampilkan_nama(){
@@ -17,5 +24,20 @@ class barang{
         return "Jenis : ".$this->stok_barang;
     }
 }
+
+class pembayaran{
+    //property
+    public $tgl_bayar,$jumlah_tagihan;
+}
+
+class lunas extends pembayaran{
+    public $jumlah_bayar, $status;
+}
+
+class kredit extends pembayaran{
+    public $jumlah_kredit,$bunga;
+}
+
+echo barang::stok_barang;
 
 ?>
